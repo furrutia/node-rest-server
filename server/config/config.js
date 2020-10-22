633842c0acbf3f66410be3e71c49dbf345571cdd
Federico <fede.urrutia@hotmail.com>
2020-10-22 19:27:28 -0300

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,24 @@
 //  PUERTO
 // ====================================
 process.env.PORT = process.env.PORT || 3000;
+
+
+
+// ====================================
+//  ENTORNO
+// ====================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ====================================
+//  BASE DE DATOS
+// ====================================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://useradmin:Of8wZnuLbii1HTOL@cluster0.vitbc.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser')
 const port = process.env.PORT;
@@ -11,42 +13,29 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-});
+const connectDB = async(urlDB) => {
 
-app.post('/usuario', function(req, res) {
+    const resp = await mongoose.connect(urlDB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    }, (err, resp) => {
 
-    let body = req.body;
+        if (err) throw err;
 
-    if (body.nombre == undefined) {
+        console.log(`resp=> base de datos ONLINE`);
 
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
     });
-});
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-});
+}
+
+// connectDB(`mongodb://localhost:27017/cafe`);
+connectDB(process.env.URLDB);
+
+
 
 app.listen(port, () => {
     console.log(`Escuchando el puerto ${port}`);
