69fc2f16c22e975ae6ccb11e4cddabdafae325ba
Federico <fede.urrutia@hotmail.com>
2020-10-22 20:56:03 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://useradmin:Of8wZnuLbii1HTOL@cluster0.vitbc.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
